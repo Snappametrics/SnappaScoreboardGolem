a263914023f5062ac6a39db403f6aba476bf3d54
mdewey131 <michael.dewey131@gmail.com>
2022-05-11 22:34:47 -0700

    Adds the scoring and defensive team names to the app server module (no longer hard-coded in the modal)

--- R/app_server.R
+++ R/app_server.R
@@ -7,8 +7,13 @@
 app_server <- function( input, output, session ) {
   # Your application server logic 
   observeEvent(input$`score-button`, {
-    shiny::showModal(mod_score_input_shinipsum_ui('team_scored'))
-                   
+    shiny::showModal(
+      mod_score_input_shinipsum_ui(id = 'A', 
+                                   scoring_team_names = c('Dewey', 'Mark', 'Matthew', NULL),
+                                   defense_team_names = c('Paul', 'Ryan', 'Gabe', 'Sugmadic')
+      )
+      )
+    mod_score_input_shinipsum_server('A')               
   })
   
 }
