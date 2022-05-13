47612a6f5a5728fc39cd04022ace2e12d052e15c
mdewey131 <michael.dewey131@gmail.com>
2022-05-13 11:31:44 -0700

    Adds a mess of changes to get a functional sass color picker

--- R/app_server.R
+++ R/app_server.R
@@ -6,7 +6,7 @@
 #' @noRd
 app_server <- function( input, output, session ) {
   # Your application server logic 
-  observeEvent(input$`score-button`, {
+  observeEvent(input$score_button_a, {
     shiny::showModal(
       mod_score_input_shinipsum_ui(id = 'A', 
                                    scoring_team_names = c('Dewey', 'Mark', 'Matthew', NULL),
@@ -14,6 +14,38 @@ app_server <- function( input, output, session ) {
       )
       )
     mod_score_input_shinipsum_server('A')               
+  })
+  
+  observeEvent(input$score_button_b, {
+    shiny::showModal(
+      mod_score_input_shinipsum_ui(id = 'B', 
+                                   scoring_team_names = c('Paul', 'Ryan', 'Gabe', 'Sugmadic'),
+                                   defense_team_names = c('Dewey', 'Mark', 'Matthew', NULL)
+      )
+    )
+    mod_score_input_shinipsum_server('B')               
+  })
+  
+  team_colors = reactive({
+    list(
+      color_a = input$main_color_a,
+      color_b = input$main_color_b
+    )
+  })
+  
+  sass_input = reactive({
+    list(
+      team_colors(),
+      sass::sass_file('inst/app/www/team_colors.scss')
+    )
+  })
+  
+  compiled_css = reactive({
+    sass::sass(sass_input())
+  })
+  
+  output$team_colors = renderUI({
+    tags$head(tags$style(compiled_css()))
   })
   
 }
